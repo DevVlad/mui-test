24c18859a435a68c38c6669b05c4a9b722f90001
DevVlad <krejciv.zcu@gmail.com>
2016-08-19 12:50:50 +0200

    calc v0.0.1 - need to improve

--- src/components/NumberInput.js
+++ src/components/NumberInput.js
@@ -4,26 +4,44 @@ import TextField from 'material-ui/TextField';
 import { transformProps } from './utils/material.js';
 import PureComponent from 'react-pure-render/component';
 
+const MATH_OPERATORS = ['*', '+', '/', '-'];
+
+const findMatchesInArray = (array, searched) => {
+	let match = [];
+	array.forEach( (elem, index) => {
+		const re = new RegExp(`[\\${elem}]`);
+		const founded = re.test(searched.toString());
+		if (founded && match.indexOf(elem) === -1) match.push(elem);
+	});
+	return match;
+};
+
 //searching for original parser in string - if non return undefined
-const exploreParsers = (arrayOfParsers) => {
+const getUniqueElemFromArray = (array) => {
 	let oneAndOnlyOriginalParser = undefined;
-	arrayOfParsers.forEach( parser => {
-		const poppedParser = arrayOfParsers.pop(parser);
+	array.forEach( parser => {
+		const poppedParser = array.pop(parser);
 		let validateUniquity = [];
-		for (let i = 0; i < arrayOfParsers.length; i++) {
-			if (arrayOfParsers[i] !== poppedParser) validateUniquity.push(true);
-		};
-		if (validateUniquity.length === arrayOfParsers.length) oneAndOnlyOriginalParser = poppedParser;
+		array.forEach( p => {
+			if (p !== poppedParser) validateUniquity.push(true);
+		});
+		if (validateUniquity.length === array.length) oneAndOnlyOriginalParser = poppedParser;
 	});
 	return oneAndOnlyOriginalParser;
 };
 
-const transformToNumber = (string, decimalParser) => {
-	let usedParsersInStrings = [];
+const getParsersFromString = (string) => {
+	let parsers = [];
 	string.split('').forEach(subString => {
-		if (/(\D)/.test(subString)) usedParsersInStrings.push(subString);
+		if (/(\D)/.test(subString)) parsers.push(subString);
 	});
-	let output = undefined;
+	return parsers;
+};
+
+const transformToNumber = (string, decimalParser) => {
+	let usedParsersInStrings = getParsersFromString(string);
+	// try to calculate string if it is possible
+	let output = string;
 	// only one parser in string
 	if (usedParsersInStrings.length === 1) {
 		const parser = usedParsersInStrings[0];
@@ -34,20 +52,20 @@ const transformToNumber = (string, decimalParser) => {
 		// more than 1 parser in string
 		let localOutput = string;
 		let decimalParserFound = false;
-		const originalParserFound = exploreParsers(usedParsersInStrings);
+		const uniqueParserFromUsed = getUniqueElemFromArray(usedParsersInStrings);
 		usedParsersInStrings.forEach( (parser, index) => {
-			if (parser !== decimalParser && parser !== originalParserFound) {
+			if (parser !== decimalParser && parser !== uniqueParserFromUsed) {
 				//case: parser is ignored, because is not original in string, or is not decimalParser
 				localOutput = localOutput.split(parser).join('');
 			} else localOutput = localOutput.split(parser).join('.');
 		});
 		//case: no decimalParser found in string, but one original parser found
-		if (!decimalParserFound && originalParserFound) {
-			localOutput = localOutput.split(originalParserFound).join('.');
+		if (!decimalParserFound && uniqueParserFromUsed) {
+			localOutput = localOutput.split(uniqueParserFromUsed).join('.');
 		}
 		output = localOutput;
 	}
-	return parseFloat(output);
+	return parseFloat(output) || undefined;
 };
 
 class NumberInput extends PureComponent {
@@ -70,6 +88,7 @@ class NumberInput extends PureComponent {
 	constructor() {
 		super();
 		this.state = {value: ''};
+		this.calcString = [];
 	}
 
 	componentWillMount() {
@@ -83,15 +102,22 @@ class NumberInput extends PureComponent {
 	componentWillReceiveProps(newProps) {
 		if (newProps.value !== this.props.value) {
 			this.setState({
-				value: new Intl.NumberFormat(newProps.locale || this.props.locale).format(newProps.value),
+				value: isNaN(newProps.value) ? 0 : new Intl.NumberFormat(newProps.locale || this.props.locale).format(newProps.value),
 				isTyping: undefined
 			});
 		}
 	}
 
 	handleOnBlur() {
-		const validatedOutput = transformToNumber(this.state.value, this.state.decimalParser);
-		this.props.onChange(validatedOutput);
+		const { value, decimalParser, isTyping } = this.state;
+		if (value !== 0) {
+			const calculated = this.tryCalculateString(value, getParsersFromString(value));
+			if (calculated && isTyping) {
+				this.props.onChange(calculated);
+			} else {
+				this.props.onChange(transformToNumber(value, decimalParser));
+			}
+		}
 	}
 
 	handleOnChange(e) {
@@ -99,6 +125,16 @@ class NumberInput extends PureComponent {
 			value: e.target.value,
 			isTyping: true
 		});
+	}
+
+	tryCalculateString = (string, usedParsersInStrings) => {
+		const foundParsers = findMatchesInArray(usedParsersInStrings, MATH_OPERATORS);
+		try {
+			return eval(string);
+		} catch (e) {
+			// TODO: calculate input
+			return undefined;
+		}
 	}
 
 	render() {
